9fdedfb20771fd9d3452a977dd6966381b94a931
Usman Malik <malik.usman.ek@gmail.com>
2018-11-29 16:53:59 +0400

    gas was the issue :/

--- truffle.js
+++ truffle.js
@@ -38,6 +38,10 @@ module.exports = {
       from: "0x74AC9Db9a5e35fd32738F1E603db80E67eB49E0A", // default address to use for any transaction Truffle makes during migrations
       network_id: "4",
       gas: 4612388 // Gas limit used for deploys
+    },solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
